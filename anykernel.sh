5daa8d9272beeb6f0cbdfa20b2c71d5bf224d54e
Willtanoe <willtanu86@gmail.com>
2022-10-29 19:23:30 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Avalanche Kernel by willtanoe
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=courbet
+device.name2=courbetin
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
